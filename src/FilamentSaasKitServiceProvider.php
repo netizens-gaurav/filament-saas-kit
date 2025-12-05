fb10450cebe56eb183ca94e8a68947661e383fa7
netizens-gaurav <156741180+netizens-gaurav@users.noreply.github.com>
2025-12-05 10:00:14 +0000

    Fix styling

--- src/FilamentSaasKitServiceProvider.php
+++ src/FilamentSaasKitServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use Netizens\FilamentSaasKit\Commands\FilamentSaasKitCommand;
+use Netizens\FilamentSaasKit\Testing\TestsFilamentSaasKit;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Netizens\FilamentSaasKit\Commands\FilamentSaasKitCommand;
-use Netizens\FilamentSaasKit\Testing\TestsFilamentSaasKit;
 
 class FilamentSaasKitServiceProvider extends PackageServiceProvider
 {
